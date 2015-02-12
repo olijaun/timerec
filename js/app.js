0374b9e4c13265468f9726d2c0ddc5b501665eee
olijaun <oliver.jaun@mobi.ch>
2015-02-12 17:28:59 +0100

    test

--- js/app.js
+++ js/app.js
@@ -2,84 +2,103 @@
 var selectedMonth;
 var selectedYear;
 
+var recordedTasks = { records: [] };
+
 window.onload = function() {
-    //document.getElementById("body").innerHTML  = 'hello world';
-
-    //$('body').html('Hello from jQuery');
-
     'use strict';
+
     console.log('Registering handler ...');
     var addBtn = document.getElementById('addBtn');
     addBtn.addEventListener('click', addRecord);
-
-
-    $('#taskTable').dynatable({features: {
-        paginate: false,
-            search: false,
-            recordCount: false,
-            perPageSelect: false
-    }});
-
-    //var datepicker = $.fn.datepicker.noConflict(); // return $.fn.datepicker to previously assigned value
-    //$.fn.bootstrapDP = datepicker;                 // give $().bootstrapDP the bootstrap-datepicker functionality
 
     $('#timepicker1').timepicker();
 
     $('#datepicker').datepicker({
         format: 'mm/dd/yyyy',
         startDate: '-3d'
-    })
+    });
 
 
+    var storageSupport = supports_html5_storage();
 
-    //$( "#datepicker" ).datepicker();
-    //
-    //$("#datepicker").datepicker({
-    //    onSelect: function(dateText) {
-    //        var day = $("#datepicker").datepicker('getDate').getDate();
-    //        selectedMonth = $("#datepicker").datepicker('getDate').getMonth() + 1;
-    //        selectedYear = $("#datepicker").datepicker('getDate').getFullYear();
-    //    }
-    //}).on("change", function() {
-    //    var day = $("#datepicker").datepicker('getDate').getDate();
-    //    selectedMonth = $("#datepicker").datepicker('getDate').getMonth() + 1;
-    //    selectedYear = $("#datepicker").datepicker('getDate').getFullYear();
-    //});
+    var taskConfiguration;
 
+    if(storageSupport) {
+        if(localStorage.taskConfiguration === null) {
+
+            var tasks = {
+                taskConfiguration: [
+                    {id: "taskA", name: "Task A"},
+                    {id: "taskB", name: "Task B"}
+                ]
+            };
+
+            var serializedTasks = JSON.stringify(tasks);
+            console.log(serializedTasks);
+            localStorage.taskConfiguration = JSON.stringify(serializedTasks);
+        }
+
+        var taskString = localStorage.taskConfiguration;
+        taskConfiguration = JSON.parse(taskString);
+    }
+
+    var taskList = taskConfiguration.taskConfiguration;
+
+    for (var index = 0; index < taskList.length; index++) {
+        var task = taskList[index];
+        $("#task").append("<option value='" + task.id + "'>" + task.name + "</option>");
+        console.log(taskList[index]);
+    }
 };
+
+function loadMonth() {
+
+}
 
 function addRecord() {
     'use strict';
 
-    var dynatable = $('#taskTable').data('dynatable');
+    var dayVal = $("#datepicker").datepicker('getDate').getDate();
+    var timeVal = "" + $("#timepicker1").val();
+    var idVal = $("#task").val();
 
-    //$( "#datepicker" ).datepicker().getDay();
-    var day = "" + $("#datepicker").datepicker('getDate').getDate();
-    var time = "" + $("#timepicker1").val();
-    var task = "" + $("#task").val();
-    var newRecord = {day: day, start: time, task: '<a href="bla">' + task + '</a>'};
-    dynatable.settings.dataset.records.push(newRecord);
-    //dynatable.records.updateFromJson({records: myRecords});
-    //dynatable.records.init();
-    dynatable.process();
+    recordedTasks.records.push({ taskId: idVal, day: dayVal, time: timeVal});
 
-    //var t = $('#example').DataTable();
+    updateTable(recordedTasks);
 
-    //t.row.add( [
-    //    'A',
-    //    'A',
-    //    'A'
-    //] ).draw();
-
-    //var table = $('#taskTable')[0];
-    //var row = table.insertRow(-1);
-    //var cell1 = row.insertCell(0);
-    //var cell2 = row.insertCell(1);
-    //var cell3 = row.insertCell(2);
-    //cell1.innerHTML = "Z";
-    //cell2.innerHTML = "A";
-    //cell3.innerHTML = "B";
-
-    //dynatable.dom.sort();
 }
 
+function updateTable(list) {
+    var table = $('#taskTable')[0];
+
+    //$("#taskTable tbody tr").remove();
+
+    while(table.rows.length > 0) {
+        table.deleteRow(0);
+    }
+
+    var records = recordedTasks.records;
+
+    for (var index = 0; index < records.length; index++) {
+        console.log(records.length);
+        var rec = records[index];
+        //$("#task").append("<option value='" + task.id + "'>" + task.name + "</option>");
+        //console.log(taskList[index]);
+
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = rec.day;
+        cell2.innerHTML = rec.time;
+        cell3.innerHTML = rec.taskId;
+    }
+}
+
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window.localStorage !== null;
+    } catch (e) {
+        return false;
+    }
+}
